8929bf0c7238d21a88773b922d1f176849c9e756
BATMAN <apprentice@dbc-chicago-34.local>
2014-02-07 13:52:46 -0600

    We did it!

--- card_controller.rb
+++ card_controller.rb
@@ -26,10 +26,8 @@ class CardController
 
   def pull_card
     a = deck.pull_random_card
-    # p a
     self.definition_store = a[1]
     self.term_store = a[0]
-    # puts term_store.downcase
   end
 end
 
