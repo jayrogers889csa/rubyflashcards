38481aafd1a58aa57316c356b51c4b9e8860d142
Apprentice <apprentice@dbc-chicago-026.local>
2014-02-09 18:17:42 -0600

    Condensed the load_cards method and renamed the options hash to be more descriptive

--- deck.rb
+++ deck.rb
@@ -9,13 +9,11 @@ class Deck
     load_cards
   end
 
-  def load_cards
+  def load_cards #eliminated creating the slices array and then having to iterate over that to create new Card objects. Just grabbed the slices from the text file and for each slice created a new Card object with a hash containing the term and defintion attributes.
     file =  File.open(filename)
-    slices = []
-    file.readlines.each_slice(3) { |slice| slices << slice }
-    slices.each do |slice|
-      hash = {:term => slice[1].chomp, :definition => slice[0].chomp}
-      self.cards << new_object(Card, hash)
+    file.readlines.each_slice(3) do |slice|
+      card_attributes = {:term => slice[1].chomp, :definition => slice[0].chomp}
+      self.cards << new_object(Card, card_attributes)
     end
   end
 
@@ -24,23 +22,24 @@ class Deck
   end
 
   def delete_card_from_deck(term_to_delete)
-    self.cards.delete_if { |card| card.options[:term] == term_to_delete }
+    self.cards.delete_if { |card| card.attributes[:term] == term_to_delete }
   end
 
   def pull_random_card
     random_card = self.cards.sample
-    random_card.options
+    random_card.attributes
   end
 
 end
 
 class Card
 
-  attr_reader :options
+  attr_reader :attributes
 
-  def initialize(options)
-    @options = options
+  def initialize(attributes)
+    @attributes = attributes
   end
 
 end
 
+#RENAMED OPTIONS AND HASH TO BE MORE DESCRIPTIVE VARIABLE AND HASH NAMES (card_attributes and attributes(hash))
